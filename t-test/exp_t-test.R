9a3e241d0ae112693e54ada9cd2c33b52b0957c8
Edgar Dobriban <dobriban@users.noreply.github.com>
2022-05-01 14:02:36 -0400

    t-test

--- t-test/exp_t-test.R
+++ t-test/exp_t-test.R
@@ -1,89 +1,128 @@
+# Experiment with mean and permutation version
 # Generating data
+
+#options: "t" or "mean"
+#stat_type = "mean"
+stat_type = "t"
+
 set.seed(2)
 
 alpha <- 0.05
 p <- 10
+
+
 num_mu <- 20
-mus <- seq(0, 3, 3 / (num_mu-1))
+mus <- seq(0, 3, 3 / (num_mu - 1))
+# num_mu <- 1
+# mus <- 0
+
 
 pow_t = rep(0, num_mu)
 pow_perm =  rep(0, num_mu)
 
-num_sim <- 100#1000
+num_sim <- 1000#1000
 num_perm <- 1000
 
 #Permutation test fn
-permutation.test <- function(X1, X2, num_perm) {
-  permuted_stat = c()
-  result = 0
-  for (i in 1:num_perm) {
-    X <- c(X1, X2)
-    Xn <- split(sample(X), rep(1:2, c(p, p)))
-    tt <- t.test(Xn[[1]], Xn[[2]], var.equal = T)
-    permuted_stat[i] = tt$statistic
-  }
-  pval = sum(permuted_stat > original_stat) / (num_perm + 1)
-  return(list(pval, permuted_stat))
+one.test <- function(ind, y) {
+  xstar <- sample(ind)
+  res <- stat(y[xstar == 1], y[xstar == 0])
+  return(res)
 }
 
-one.test <- function(ind,y) {
-  xstar<-sample(ind)
-  t.test(y[xstar==1], y[xstar==0], var.equal = T)$statistic
+if (stat_type == "t") {
+  stat <- function(x1, x2) {
+    p <- length(x1)
+    tt <- t.test(x1, x2, var.equal = T)
+    s_det <- tt$statistic
+    p_det <- tt$p.value
+    return(c(s_det, p_det))
+  }
 }
+if (stat_type == "mean") {
+  stat <- function(x1, x2) {
+    p <- length(X1)
+    s_det <- (mean(x1) - mean(x2)) / sqrt(2 / p)
+    p_det <- 1 - pnorm(s_det)
+    return(c(s_det, p_det))
+  }
+}
+
 
 for (i in 1:num_mu) {
   mu <- mus[i]
   
   rej_t <- rep(0, num_sim)
   rej_perm <- rep(0, num_sim)
-
+  
   for (j in 1:num_sim) {
     X1 <- rnorm(p) + mu * rep(1, p)
     X2 <- rnorm(p)
-    tt <- t.test(X1, X2, var.equal = T)
-    original_stat <- tt$statistic
-    rej_t[j] <- 1 * (tt$p.value < alpha)    
+    s <- stat(X1, X2)
+    original_stat <- s[1]
+    rej_t[j] <- 1 * (s[2] < alpha)
     
-    #T-test: version 1
-    # perm_tt <- permutation.test(X1, X2, num_perm)
-    # pval <- perm_tt[[1]]
-    
-    #T-test: version 2
+    #T-test
     #this code partly inspired by https://faculty.washington.edu/kenrice/sisg/SISG-08-06.pdf
-    X <- c(X1,X2)
-    ind <- c(rep(1,p),rep(0,p))
+    X <- c(X1, X2)
+    ind <- c(rep(1, p), rep(0, p))
     perm_stats <- replicate(num_perm, one.test(ind, X))
-    pval <- mean(perm_stats > original_stat)
+    pval <- (sum(perm_stats >= original_stat) + 1) / (num_perm + 1)
     
     
     rej_perm[j] <- 1 * (pval < alpha)
   }
   
-    pow_t[i] = mean(rej_t)
-    pow_perm[i] = mean(rej_perm)
+  pow_t[i] = mean(rej_t)
+  pow_perm[i] = mean(rej_perm)
   
 }
 
+
+draw <- 0
+if (draw == 1) {
+  hist(pvals, 100)
+}
+
 ########
-#png(file="t_exp.png", width=600, height=350)
-png(file="t_exp_2.png", width=600, height=350)
-plot(x=mus, y=pow_t, type="l", lty=1, ylim=c(0,1),
-     axes=T, bty="n", xaxs="i", yaxs="i",
-     xlab="mu", ylab="Power")
+if (stat_type == "t") {
+  png(file = "t_exp.png",
+      width = 600,
+      height = 350)
+}
+
+if (stat_type == "mean") {
+  png(file = "mean_exp.png",
+      width = 600,
+      height = 350)
+}
+
+
+plot(
+  x = mus,
+  y = pow_t,
+  type = "l",
+  lty = 1,
+  ylim = c(0, 1),
+  axes = T,
+  bty = "n",
+  xaxs = "i",
+  yaxs = "i",
+  xlab = "mu",
+  ylab = "Power"
+)
 
 # plot dashed line
-lines(x=mus, y=pow_perm, lty=2)
-
-# add axes
-#axis(side=1, labels=, at=1:num_mu)
-#axis(side=2, at=seq(0,1,10), las=1)
+lines(x = mus, y = pow_perm, lty = 2)
 
 # add legend
-par(xpd=TRUE)
-legend(x=1.5, y=0.2, legend=c("Deterministic", "Permutation"), lty=1:2, box.lty=0, ncol=2)
+par(xpd = TRUE)
+legend(
+  x = 1.5,
+  y = 0.2,
+  legend = c("Deterministic", "Permutation"),
+  lty = 1:2,
+  box.lty = 0,
+  ncol = 2
+)
 dev.off()
-
-
-
-
-
--- t-test/expt_t-test.m
+++ t-test/expt_t-test.m
@@ -1,5 +1,10 @@
 % evaluatee rand test (two-sample test)
 cd('C:\Dropbox\Projects\Randomization test\exp\t-test')
+
+%choose the test type: "mean": difference in means; "t": t-test
+%test_type = "mean";
+test_type = "t";
+
 %num_mu = 1;
 num_mu = 20;
 
@@ -29,24 +34,39 @@ for i=1:num_mu
         X1 = ones(n1,1)*S1+ N1;
         N2 = randn(n2,1);
         X2 = N2;
-        T = t2(X1,X2,n1,n2);
-        
         
         %Deterministic
-        rej(1,i,j)=ttest2(X1,X2);
+        if (test_type == "t")
+            stat = t2(X1,X2,n1,n2);
+            rej(1,i,j)=ttest2(X1,X2);
+        end
+        if (test_type == "mean")
+            stat = (mean(X1)-mean(X2))/sqrt(1/n1+1/n2);
+            pv = 1- normcdf(stat);
+            rej(1,i,j)= (pv<alpha);
+        end
+        
+        
         
         %Randomized
-        gT = zeros(K,1);
+        perm_stat = zeros(K,1);
         for k=1:K
             X = [X1;X2];
             gX = X(randperm(length(X)));
             gX1 = gX(1:length(X1));
             gX2 = gX(length(X1)+1:length(X));
-            gT(k) =  t2(gX1,gX2,n1,n2);
+            
+            if (test_type == "t")
+                perm_stat(k) =  t2(gX1,gX2,n1,n2);
+            end
+            if (test_type == "mean")
+                perm_stat(k) =  (mean(gX1)-mean(gX2))/sqrt(1/n1+1/n2);
+            end
+            
         end
-        x  = sort(gT);
+        x  = sort(perm_stat);
         thresh = x(index);
-        if T> thresh
+        if stat> thresh
             rej(2,i,j)=1;
         end
     end
@@ -69,9 +89,15 @@ ylim([0, 1]);
 xlim([0, max(mu)]);
 set(gca,'fontsize',18)
 grid on;
-filename = sprintf('t-test_pow_n_%d_K_%d_num_mu_%d_nrep_%d.png', n1, K, num_mu, num_rep);
+if (test_type == "t")
+    filename = sprintf('t-test_pow_n_%d_K_%d_num_mu_%d_nrep_%d.png', n1, K, num_mu, num_rep);
+end
+if (test_type == "mean")
+    filename = sprintf('mean_pow_n_%d_K_%d_num_mu_%d_nrep_%d.png', n1, K, num_mu, num_rep);
+end
+
 saveas(gcf, filename);
 
 %% optionally, save data to file
-save('t-test-exp.mat')
+%save('t-test-exp.mat')
 
